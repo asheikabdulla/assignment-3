1099e6682e0df34f9f5aeb81e33a1f71d1412e32
asheikabdulla <91787538+asheikabdulla@users.noreply.github.com>
2021-10-17 03:25:40 -0700

    Add files via upload

---
+++ String inside the function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def reverser(string):
+    string=str(string)
+    print(string[::-1])
+
+
+# In[2]:
+
+
+sample_string = "1234abcd"
+reverser(sample_string)
+
+
+# In[ ]:
+
+
+
+
